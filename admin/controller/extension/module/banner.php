cd1131c6e1d8436bc785a7ea98ec0b082db894a7
ex <donotstoptry@gmail.com>
2025-05-27 23:44:44 +0400

    you can copy modules now

--- admin/controller/extension/module/banner.php
+++ admin/controller/extension/module/banner.php
@@ -1,162 +1,166 @@
 <?php
 class ControllerExtensionModuleBanner extends Controller {
-	private $error = array();
+  private $error = array();
 
-	public function index() {
-		$this->load->language('extension/module/banner');
+  public function index() {
+    $this->load->language('extension/module/banner');
 
-		$this->document->setTitle($this->language->get('heading_title'));
+    $this->document->setTitle($this->language->get('heading_title'));
 
-		$this->load->model('setting/module');
+    $this->load->model('setting/module');
 
-		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-			if (!isset($this->request->get['module_id'])) {
-				$module_id = $this->model_setting_module->addModule('banner', $this->request->post);
-			} else {
-				$module_id = $this->request->get['module_id'];
-				$this->model_setting_module->editModule($this->request->get['module_id'], $this->request->post);
-			}
+    if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+      if (!isset($this->request->get['module_id'])) {
+        $module_id = $this->model_setting_module->addModule('banner', $this->request->post);
+      } else {
+        $module_id = $this->request->get['module_id'];
+        $this->model_setting_module->editModule($this->request->get['module_id'], $this->request->post);
+      }
 
-			$this->session->data['success'] = $this->language->get('text_success');
+      $this->session->data['success'] = $this->language->get('text_success');
 
-			$url = '';
+      $url = '';
 
-			if(isset($this->request->post['apply']) && $this->request->post['apply'] == 1) {
-				$url .= '&module_id=' . $module_id;
-				$this->response->redirect($this->url->link('extension/module/banner', 'user_token=' . $this->session->data['user_token'] . $url, true));
-			} else {
-				$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true));
-			}
-		}
+      if (isset($this->request->post['apply']) && $this->request->post['apply'] == 1) {
+        $url .= '&module_id=' . $module_id;
+        $this->response->redirect($this->url->link('extension/module/banner', 'user_token=' . $this->session->data['user_token'] . $url, true));
+      } else {
+        $this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true));
+      }
+    }
 
-		if (isset($this->error['warning'])) {
-			$data['error_warning'] = $this->error['warning'];
-		} else {
-			$data['error_warning'] = '';
-		}
+    if (isset($this->error['warning'])) {
+      $data['error_warning'] = $this->error['warning'];
+    } else {
+      $data['error_warning'] = '';
+    }
 
-		if (isset($this->error['name'])) {
-			$data['error_name'] = $this->error['name'];
-		} else {
-			$data['error_name'] = '';
-		}
+    if (isset($this->error['name'])) {
+      $data['error_name'] = $this->error['name'];
+    } else {
+      $data['error_name'] = '';
+    }
 
-		if (isset($this->error['width'])) {
-			$data['error_width'] = $this->error['width'];
-		} else {
-			$data['error_width'] = '';
-		}
+    if (isset($this->error['width'])) {
+      $data['error_width'] = $this->error['width'];
+    } else {
+      $data['error_width'] = '';
+    }
 
-		if (isset($this->error['height'])) {
-			$data['error_height'] = $this->error['height'];
-		} else {
-			$data['error_height'] = '';
-		}
+    if (isset($this->error['height'])) {
+      $data['error_height'] = $this->error['height'];
+    } else {
+      $data['error_height'] = '';
+    }
 
-		$data['breadcrumbs'] = array();
+    $data['breadcrumbs'] = array();
 
-		$data['breadcrumbs'][] = array(
-			'text' => $this->language->get('text_home'),
-			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
-		);
+    $data['breadcrumbs'][] = array(
+      'text' => $this->language->get('text_home'),
+      'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+    );
 
-		$data['breadcrumbs'][] = array(
-			'text' => $this->language->get('text_extension'),
-			'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true)
-		);
+    $data['breadcrumbs'][] = array(
+      'text' => $this->language->get('text_extension'),
+      'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true)
+    );
 
-		if (!isset($this->request->get['module_id'])) {
-			$data['breadcrumbs'][] = array(
-				'text' => $this->language->get('heading_title'),
-				'href' => $this->url->link('extension/module/banner', 'user_token=' . $this->session->data['user_token'], true)
-			);
-		} else {
-			$data['breadcrumbs'][] = array(
-				'text' => $this->language->get('heading_title'),
-				'href' => $this->url->link('extension/module/banner', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true)
-			);
-		}
+    if (!isset($this->request->get['module_id'])) {
+      $data['breadcrumbs'][] = array(
+        'text' => $this->language->get('heading_title'),
+        'href' => $this->url->link('extension/module/banner', 'user_token=' . $this->session->data['user_token'], true)
+      );
+    } else {
+      $data['breadcrumbs'][] = array(
+        'text' => $this->language->get('heading_title'),
+        'href' => $this->url->link('extension/module/banner', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true)
+      );
+    }
 
-		if (!isset($this->request->get['module_id'])) {
-			$data['action'] = $this->url->link('extension/module/banner', 'user_token=' . $this->session->data['user_token'], true);
-		} else {
-			$data['action'] = $this->url->link('extension/module/banner', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true);
-		}
+    if (!isset($this->request->get['module_id'])) {
+      $data['action'] = $this->url->link('extension/module/banner', 'user_token=' . $this->session->data['user_token'], true);
+    } else {
+      $data['action'] = $this->url->link('extension/module/banner', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true);
+    }
 
-		$data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true);
+    $data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true);
 
-		if (isset($this->request->get['module_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
-			$module_info = $this->model_setting_module->getModule($this->request->get['module_id']);
-		}
+    if (isset($this->request->get['module_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+      $module_info = $this->model_setting_module->getModule($this->request->get['module_id']);
+    }
 
-		if (isset($this->request->post['name'])) {
-			$data['name'] = $this->request->post['name'];
-		} elseif (!empty($module_info)) {
-			$data['name'] = $module_info['name'];
-		} else {
-			$data['name'] = '';
-		}
+    if (isset($this->request->get['copy_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+      $module_info = $this->model_setting_module->getModule($this->request->get['copy_id']);
+    }
 
-		if (isset($this->request->post['banner_id'])) {
-			$data['banner_id'] = $this->request->post['banner_id'];
-		} elseif (!empty($module_info)) {
-			$data['banner_id'] = $module_info['banner_id'];
-		} else {
-			$data['banner_id'] = '';
-		}
+    if (isset($this->request->post['name'])) {
+      $data['name'] = $this->request->post['name'];
+    } elseif (!empty($module_info)) {
+      $data['name'] = $module_info['name'];
+    } else {
+      $data['name'] = '';
+    }
 
-		$this->load->model('design/banner');
+    if (isset($this->request->post['banner_id'])) {
+      $data['banner_id'] = $this->request->post['banner_id'];
+    } elseif (!empty($module_info)) {
+      $data['banner_id'] = $module_info['banner_id'];
+    } else {
+      $data['banner_id'] = '';
+    }
 
-		$data['banners'] = $this->model_design_banner->getBanners();
+    $this->load->model('design/banner');
 
-		if (isset($this->request->post['width'])) {
-			$data['width'] = $this->request->post['width'];
-		} elseif (!empty($module_info)) {
-			$data['width'] = $module_info['width'];
-		} else {
-			$data['width'] = '';
-		}
+    $data['banners'] = $this->model_design_banner->getBanners();
 
-		if (isset($this->request->post['height'])) {
-			$data['height'] = $this->request->post['height'];
-		} elseif (!empty($module_info)) {
-			$data['height'] = $module_info['height'];
-		} else {
-			$data['height'] = '';
-		}
+    if (isset($this->request->post['width'])) {
+      $data['width'] = $this->request->post['width'];
+    } elseif (!empty($module_info)) {
+      $data['width'] = $module_info['width'];
+    } else {
+      $data['width'] = '';
+    }
 
-		if (isset($this->request->post['status'])) {
-			$data['status'] = $this->request->post['status'];
-		} elseif (!empty($module_info)) {
-			$data['status'] = $module_info['status'];
-		} else {
-			$data['status'] = '';
-		}
+    if (isset($this->request->post['height'])) {
+      $data['height'] = $this->request->post['height'];
+    } elseif (!empty($module_info)) {
+      $data['height'] = $module_info['height'];
+    } else {
+      $data['height'] = '';
+    }
 
-		$data['header'] = $this->load->controller('common/header');
-		$data['column_left'] = $this->load->controller('common/column_left');
-		$data['footer'] = $this->load->controller('common/footer');
+    if (isset($this->request->post['status'])) {
+      $data['status'] = $this->request->post['status'];
+    } elseif (!empty($module_info)) {
+      $data['status'] = $module_info['status'];
+    } else {
+      $data['status'] = '';
+    }
 
-		$this->response->setOutput($this->load->view('extension/module/banner', $data));
-	}
+    $data['header'] = $this->load->controller('common/header');
+    $data['column_left'] = $this->load->controller('common/column_left');
+    $data['footer'] = $this->load->controller('common/footer');
 
-	protected function validate() {
-		if (!$this->user->hasPermission('modify', 'extension/module/banner')) {
-			$this->error['warning'] = $this->language->get('error_permission');
-		}
+    $this->response->setOutput($this->load->view('extension/module/banner', $data));
+  }
 
-		if ((utf8_strlen($this->request->post['name']) < 3) || (utf8_strlen($this->request->post['name']) > 64)) {
-			$this->error['name'] = $this->language->get('error_name');
-		}
+  protected function validate() {
+    if (!$this->user->hasPermission('modify', 'extension/module/banner')) {
+      $this->error['warning'] = $this->language->get('error_permission');
+    }
 
-		if (!$this->request->post['width']) {
-			$this->error['width'] = $this->language->get('error_width');
-		}
+    if ((utf8_strlen($this->request->post['name']) < 3) || (utf8_strlen($this->request->post['name']) > 64)) {
+      $this->error['name'] = $this->language->get('error_name');
+    }
 
-		if (!$this->request->post['height']) {
-			$this->error['height'] = $this->language->get('error_height');
-		}
+    if (!$this->request->post['width']) {
+      $this->error['width'] = $this->language->get('error_width');
+    }
 
-		return !$this->error;
-	}
+    if (!$this->request->post['height']) {
+      $this->error['height'] = $this->language->get('error_height');
+    }
+
+    return !$this->error;
+  }
 }
--- admin/controller/extension/module/blog_latest.php
+++ admin/controller/extension/module/blog_latest.php
@@ -3,172 +3,176 @@
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
 class ControllerExtensionModuleBlogLatest extends Controller {
-	private $error = array();
+  private $error = array();
 
-	public function index() {
-		$this->load->language('extension/module/blog_latest');
+  public function index() {
+    $this->load->language('extension/module/blog_latest');
 
-		$this->document->setTitle($this->language->get('heading_title'));
+    $this->document->setTitle($this->language->get('heading_title'));
 
-		$this->load->model('setting/module');
+    $this->load->model('setting/module');
 
-		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-			if (!isset($this->request->get['module_id'])) {
-				$module_id = $this->model_setting_module->addModule('blog_latest', $this->request->post);
-			} else {
-				$module_id = $this->request->get['module_id'];
-				$this->model_setting_module->editModule($this->request->get['module_id'], $this->request->post);
-			}
+    if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+      if (!isset($this->request->get['module_id'])) {
+        $module_id = $this->model_setting_module->addModule('blog_latest', $this->request->post);
+      } else {
+        $module_id = $this->request->get['module_id'];
+        $this->model_setting_module->editModule($this->request->get['module_id'], $this->request->post);
+      }
 
-			$this->cache->delete('article');
+      $this->cache->delete('article');
 
-			$this->session->data['success'] = $this->language->get('text_success');
+      $this->session->data['success'] = $this->language->get('text_success');
 
-			$url = '';
+      $url = '';
 
-			if(isset($this->request->post['apply']) && $this->request->post['apply'] == 1) {
-				$url .= '&module_id=' . $module_id;
-				$this->response->redirect($this->url->link('extension/module/blog_latest', 'user_token=' . $this->session->data['user_token'] . $url, true));
-			} else {
-				$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true));
-			}
-		}
+      if (isset($this->request->post['apply']) && $this->request->post['apply'] == 1) {
+        $url .= '&module_id=' . $module_id;
+        $this->response->redirect($this->url->link('extension/module/blog_latest', 'user_token=' . $this->session->data['user_token'] . $url, true));
+      } else {
+        $this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true));
+      }
+    }
 
-		if (isset($this->error['warning'])) {
-			$data['error_warning'] = $this->error['warning'];
-		} else {
-			$data['error_warning'] = '';
-		}
+    if (isset($this->error['warning'])) {
+      $data['error_warning'] = $this->error['warning'];
+    } else {
+      $data['error_warning'] = '';
+    }
 
-		if (isset($this->error['name'])) {
-			$data['error_name'] = $this->error['name'];
-		} else {
-			$data['error_name'] = '';
-		}
+    if (isset($this->error['name'])) {
+      $data['error_name'] = $this->error['name'];
+    } else {
+      $data['error_name'] = '';
+    }
 
-		if (isset($this->error['width'])) {
-			$data['error_width'] = $this->error['width'];
-		} else {
-			$data['error_width'] = '';
-		}
+    if (isset($this->error['width'])) {
+      $data['error_width'] = $this->error['width'];
+    } else {
+      $data['error_width'] = '';
+    }
 
-		if (isset($this->error['height'])) {
-			$data['error_height'] = $this->error['height'];
-		} else {
-			$data['error_height'] = '';
-		}
+    if (isset($this->error['height'])) {
+      $data['error_height'] = $this->error['height'];
+    } else {
+      $data['error_height'] = '';
+    }
 
-		$data['breadcrumbs'] = array();
+    $data['breadcrumbs'] = array();
 
-		$data['breadcrumbs'][] = array(
-			'text' => $this->language->get('text_home'),
-			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
-		);
+    $data['breadcrumbs'][] = array(
+      'text' => $this->language->get('text_home'),
+      'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+    );
 
-		$data['breadcrumbs'][] = array(
-			'text' => $this->language->get('text_extension'),
-			'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true)
-		);
+    $data['breadcrumbs'][] = array(
+      'text' => $this->language->get('text_extension'),
+      'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true)
+    );
 
-		if (!isset($this->request->get['module_id'])) {
-			$data['breadcrumbs'][] = array(
-				'text' => $this->language->get('heading_title'),
-				'href' => $this->url->link('extension/module/blog_latest', 'user_token=' . $this->session->data['user_token'], true)
-			);
-		} else {
-			$data['breadcrumbs'][] = array(
-				'text' => $this->language->get('heading_title'),
-				'href' => $this->url->link('extension/module/blog_latest', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true)
-			);
-		}
+    if (!isset($this->request->get['module_id'])) {
+      $data['breadcrumbs'][] = array(
+        'text' => $this->language->get('heading_title'),
+        'href' => $this->url->link('extension/module/blog_latest', 'user_token=' . $this->session->data['user_token'], true)
+      );
+    } else {
+      $data['breadcrumbs'][] = array(
+        'text' => $this->language->get('heading_title'),
+        'href' => $this->url->link('extension/module/blog_latest', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true)
+      );
+    }
 
-		if (!isset($this->request->get['module_id'])) {
-			$data['action'] = $this->url->link('extension/module/blog_latest', 'user_token=' . $this->session->data['user_token'], true);
-		} else {
-			$data['action'] = $this->url->link('extension/module/blog_latest', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true);
-		}
+    if (!isset($this->request->get['module_id'])) {
+      $data['action'] = $this->url->link('extension/module/blog_latest', 'user_token=' . $this->session->data['user_token'], true);
+    } else {
+      $data['action'] = $this->url->link('extension/module/blog_latest', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true);
+    }
 
-		$data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true);
+    $data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true);
 
-		if (isset($this->request->get['module_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
-			$module_info = $this->model_setting_module->getModule($this->request->get['module_id']);
-		}
+    if (isset($this->request->get['module_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+      $module_info = $this->model_setting_module->getModule($this->request->get['module_id']);
+    }
 
-		if (isset($this->request->post['name'])) {
-			$data['name'] = $this->request->post['name'];
-		} elseif (!empty($module_info)) {
-			$data['name'] = $module_info['name'];
-		} else {
-			$data['name'] = '';
-		}
+    if (isset($this->request->get['copy_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+      $module_info = $this->model_setting_module->getModule($this->request->get['copy_id']);
+    }
 
-		if (isset($this->request->post['text'])) {
-			$data['text'] = $this->request->post['text'];
-		} elseif (!empty($module_info)) {
-			$data['text'] = $module_info['text'];
-		} else {
-			$data['text'] = [];
-		}
+    if (isset($this->request->post['name'])) {
+      $data['name'] = $this->request->post['name'];
+    } elseif (!empty($module_info)) {
+      $data['name'] = $module_info['name'];
+    } else {
+      $data['name'] = '';
+    }
 
-		$this->load->model('localisation/language');
-		$data['languages'] = $this->model_localisation_language->getLanguages();
+    if (isset($this->request->post['text'])) {
+      $data['text'] = $this->request->post['text'];
+    } elseif (!empty($module_info)) {
+      $data['text'] = $module_info['text'];
+    } else {
+      $data['text'] = [];
+    }
 
-		if (isset($this->request->post['limit'])) {
-			$data['limit'] = $this->request->post['limit'];
-		} elseif (!empty($module_info)) {
-			$data['limit'] = $module_info['limit'];
-		} else {
-			$data['limit'] = 5;
-		}
+    $this->load->model('localisation/language');
+    $data['languages'] = $this->model_localisation_language->getLanguages();
 
-		if (isset($this->request->post['width'])) {
-			$data['width'] = $this->request->post['width'];
-		} elseif (!empty($module_info)) {
-			$data['width'] = $module_info['width'];
-		} else {
-			$data['width'] = 200;
-		}
+    if (isset($this->request->post['limit'])) {
+      $data['limit'] = $this->request->post['limit'];
+    } elseif (!empty($module_info)) {
+      $data['limit'] = $module_info['limit'];
+    } else {
+      $data['limit'] = 5;
+    }
 
-		if (isset($this->request->post['height'])) {
-			$data['height'] = $this->request->post['height'];
-		} elseif (!empty($module_info)) {
-			$data['height'] = $module_info['height'];
-		} else {
-			$data['height'] = 200;
-		}
+    if (isset($this->request->post['width'])) {
+      $data['width'] = $this->request->post['width'];
+    } elseif (!empty($module_info)) {
+      $data['width'] = $module_info['width'];
+    } else {
+      $data['width'] = 200;
+    }
 
-		if (isset($this->request->post['status'])) {
-			$data['status'] = $this->request->post['status'];
-		} elseif (!empty($module_info)) {
-			$data['status'] = $module_info['status'];
-		} else {
-			$data['status'] = '';
-		}
+    if (isset($this->request->post['height'])) {
+      $data['height'] = $this->request->post['height'];
+    } elseif (!empty($module_info)) {
+      $data['height'] = $module_info['height'];
+    } else {
+      $data['height'] = 200;
+    }
 
-		$data['header'] = $this->load->controller('common/header');
-		$data['column_left'] = $this->load->controller('common/column_left');
-		$data['footer'] = $this->load->controller('common/footer');
+    if (isset($this->request->post['status'])) {
+      $data['status'] = $this->request->post['status'];
+    } elseif (!empty($module_info)) {
+      $data['status'] = $module_info['status'];
+    } else {
+      $data['status'] = '';
+    }
 
-		$this->response->setOutput($this->load->view('extension/module/blog_latest', $data));
-	}
+    $data['header'] = $this->load->controller('common/header');
+    $data['column_left'] = $this->load->controller('common/column_left');
+    $data['footer'] = $this->load->controller('common/footer');
 
-	protected function validate() {
-		if (!$this->user->hasPermission('modify', 'extension/module/blog_latest')) {
-			$this->error['warning'] = $this->language->get('error_permission');
-		}
+    $this->response->setOutput($this->load->view('extension/module/blog_latest', $data));
+  }
 
-		if ((utf8_strlen($this->request->post['name']) < 3) || (utf8_strlen($this->request->post['name']) > 64)) {
-			$this->error['name'] = $this->language->get('error_name');
-		}
+  protected function validate() {
+    if (!$this->user->hasPermission('modify', 'extension/module/blog_latest')) {
+      $this->error['warning'] = $this->language->get('error_permission');
+    }
 
-		if (!$this->request->post['width']) {
-			$this->error['width'] = $this->language->get('error_width');
-		}
+    if ((utf8_strlen($this->request->post['name']) < 3) || (utf8_strlen($this->request->post['name']) > 64)) {
+      $this->error['name'] = $this->language->get('error_name');
+    }
 
-		if (!$this->request->post['height']) {
-			$this->error['height'] = $this->language->get('error_height');
-		}
+    if (!$this->request->post['width']) {
+      $this->error['width'] = $this->language->get('error_width');
+    }
 
-		return !$this->error;
-	}
+    if (!$this->request->post['height']) {
+      $this->error['height'] = $this->language->get('error_height');
+    }
+
+    return !$this->error;
+  }
 }
