deadf2f6dcc31d68d087c182ccf34cfd35515501
ex <donotstoptry@gmail.com>
2025-06-13 00:40:32 +0400

    SEO URL auto generation is done

--- admin/view/javascript/common.js
+++ admin/view/javascript/common.js
@@ -1,282 +1,311 @@
 function getURLVar(key) {
-	var value = [];
+  var value = [];
 
-	var query = String(document.location).split('?');
+  var query = String(document.location).split('?');
 
-	if (query[1]) {
-		var part = query[1].split('&');
+  if (query[1]) {
+    var part = query[1].split('&');
 
-		for (i = 0; i < part.length; i++) {
-			var data = part[i].split('=');
+    for (i = 0; i < part.length; i++) {
+      var data = part[i].split('=');
 
-			if (data[0] && data[1]) {
-				value[data[0]] = data[1];
-			}
-		}
+      if (data[0] && data[1]) {
+        value[data[0]] = data[1];
+      }
+    }
 
-		if (value[key]) {
-			return value[key];
-		} else {
-			return '';
-		}
-	}
+    if (value[key]) {
+      return value[key];
+    } else {
+      return '';
+    }
+  }
 }
 
-$(document).ready(function() {
-	//Form Submit for IE Browser
-	$('button[type=\'submit\']').on('click', function() {
-		$("form[id*='form-']").submit();
-	});
+function translit(text) {
+  const map = {
+    'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo',
+    'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm',
+    'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+    'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch',
+    'ы': 'y', 'э': 'e', 'ю': 'yu', 'я': 'ya', ' ': '-', 'ъ': '', 'ь': ''
+  };
 
-	// Highlight any found errors
-	$('.text-danger').each(function() {
-		var element = $(this).parent().parent();
+  return text.toLowerCase().replace(/[а-яёъь\s]/g, function (char) {
+    return map[char] || '';
+  }).replace(/[^a-z0-9\-]/g, '').replace(/\-+/g, '-');
+}
 
-		if (element.hasClass('form-group')) {
-			element.addClass('has-error');
-		}
-	});
+function generateSeoUrl(element, language_id) {
+  let $input = $(element);
+  let $seo_url = $(`input[name="category_seo_url[0][${language_id}]"], 
+                    input[name="product_seo_url[0][${language_id}]"],
+                    input[name="manufacturer_seo_url[0][${language_id}]"],
+                    input[name="product_seo_url[0][${language_id}]"],
+                    input[name="article_seo_url[0][${language_id}]"],
+                    input[name="information_seo_url[0][${language_id}]"]`);
 
-	// tooltips on hover
-	$('[data-toggle=\'tooltip\']').tooltip({container: 'body', html: true});
+  if (!$seo_url.val()) {
+    let seo_value = translit($input.val());
+    $seo_url.val(seo_value);
+  }
+}
 
-	// Makes tooltips work on ajax generated content
-	$(document).ajaxStop(function() {
-		$('[data-toggle=\'tooltip\']').tooltip({container: 'body'});
-	});
+$(document).ready(function () {
+  //Form Submit for IE Browser
+  $('button[type=\'submit\']').on('click', function () {
+    $("form[id*='form-']").submit();
+  });
 
-	// https://github.com/opencart/opencart/issues/2595
-	$.event.special.remove = {
-		remove: function(o) {
-			if (o.handler) {
-				o.handler.apply(this, arguments);
-			}
-		}
-	}
+  // Highlight any found errors
+  $('.text-danger').each(function () {
+    var element = $(this).parent().parent();
 
-	// tooltip remove
-	$('[data-toggle=\'tooltip\']').on('remove', function() {
-		$(this).tooltip('destroy');
-	});
+    if (element.hasClass('form-group')) {
+      element.addClass('has-error');
+    }
+  });
 
-	// Tooltip remove fixed
-	$(document).on('click', '[data-toggle=\'tooltip\']', function(e) {
-		$('body > .tooltip').remove();
-	});
+  // tooltips on hover
+  $('[data-toggle=\'tooltip\']').tooltip({ container: 'body', html: true });
 
-	$('#button-menu').on('click', function(e) {
-		e.preventDefault();
+  // Makes tooltips work on ajax generated content
+  $(document).ajaxStop(function () {
+    $('[data-toggle=\'tooltip\']').tooltip({ container: 'body' });
+  });
 
-		$('#column-left').toggleClass('active');
-	});
+  // https://github.com/opencart/opencart/issues/2595
+  $.event.special.remove = {
+    remove: function (o) {
+      if (o.handler) {
+        o.handler.apply(this, arguments);
+      }
+    }
+  }
 
-	// Set last page opened on the menu
-	$('#menu a[href]').on('click', function() {
-		sessionStorage.setItem('menu', $(this).attr('href'));
-	});
+  // tooltip remove
+  $('[data-toggle=\'tooltip\']').on('remove', function () {
+    $(this).tooltip('destroy');
+  });
 
-	if (!sessionStorage.getItem('menu')) {
-		$('#menu #dashboard').addClass('active');
-	} else {
-		// Sets active and open to selected page in the left column menu.
-		$('#menu a[href=\'' + sessionStorage.getItem('menu') + '\']').parent().addClass('active');
-	}
+  // Tooltip remove fixed
+  $(document).on('click', '[data-toggle=\'tooltip\']', function (e) {
+    $('body > .tooltip').remove();
+  });
 
-	$('#menu a[href=\'' + sessionStorage.getItem('menu') + '\']').parents('li > a').removeClass('collapsed');
+  $('#button-menu').on('click', function (e) {
+    e.preventDefault();
 
-	$('#menu a[href=\'' + sessionStorage.getItem('menu') + '\']').parents('ul').addClass('in');
+    $('#column-left').toggleClass('active');
+  });
 
-	$('#menu a[href=\'' + sessionStorage.getItem('menu') + '\']').parents('li').addClass('active');
+  // Set last page opened on the menu
+  $('#menu a[href]').on('click', function () {
+    sessionStorage.setItem('menu', $(this).attr('href'));
+  });
 
-	// Image Manager
-	$(document).on('click', 'a[data-toggle=\'image\']', function(e) {
-		var $element = $(this);
-		var $popover = $element.data('bs.popover'); // element has bs popover?
+  if (!sessionStorage.getItem('menu')) {
+    $('#menu #dashboard').addClass('active');
+  } else {
+    // Sets active and open to selected page in the left column menu.
+    $('#menu a[href=\'' + sessionStorage.getItem('menu') + '\']').parent().addClass('active');
+  }
 
-		e.preventDefault();
+  $('#menu a[href=\'' + sessionStorage.getItem('menu') + '\']').parents('li > a').removeClass('collapsed');
 
-		// destroy all image popovers
-		$('a[data-toggle="image"]').popover('destroy');
+  $('#menu a[href=\'' + sessionStorage.getItem('menu') + '\']').parents('ul').addClass('in');
 
-		// remove flickering (do not re-add popover when clicking for removal)
-		if ($popover) {
-			return;
-		}
+  $('#menu a[href=\'' + sessionStorage.getItem('menu') + '\']').parents('li').addClass('active');
 
-		$element.popover({
-			html: true,
-			placement: 'right',
-			trigger: 'manual',
-			content: function() {
-				return '<button type="button" id="button-image" class="btn btn-primary"><i class="fa fa-pencil"></i></button> <button type="button" id="button-clear" class="btn btn-danger"><i class="fa fa-trash-o"></i></button>';
-			}
-		});
+  // Image Manager
+  $(document).on('click', 'a[data-toggle=\'image\']', function (e) {
+    var $element = $(this);
+    var $popover = $element.data('bs.popover'); // element has bs popover?
 
-		$element.popover('show');
+    e.preventDefault();
 
-		$('#button-image').on('click', function() {
-			var $button = $(this);
-			var $icon   = $button.find('> i');
+    // destroy all image popovers
+    $('a[data-toggle="image"]').popover('destroy');
 
-			$('#modal-image').remove();
+    // remove flickering (do not re-add popover when clicking for removal)
+    if ($popover) {
+      return;
+    }
 
-			$.ajax({
-				url: 'index.php?route=common/filemanager&user_token=' + getURLVar('user_token') + '&target=' + $element.parent().find('input').attr('id') + '&thumb=' + $element.attr('id'),
-				dataType: 'html',
-				beforeSend: function() {
-					$button.prop('disabled', true);
-					if ($icon.length) {
-						$icon.attr('class', 'fa fa-circle-o-notch fa-spin');
-					}
-				},
-				complete: function() {
-					$button.prop('disabled', false);
+    $element.popover({
+      html: true,
+      placement: 'right',
+      trigger: 'manual',
+      content: function () {
+        return '<button type="button" id="button-image" class="btn btn-primary"><i class="fa fa-pencil"></i></button> <button type="button" id="button-clear" class="btn btn-danger"><i class="fa fa-trash-o"></i></button>';
+      }
+    });
 
-					if ($icon.length) {
-						$icon.attr('class', 'fa fa-pencil');
-					}
-				},
-				success: function(html) {
-					$('body').append('<div id="modal-image" class="modal">' + html + '</div>');
+    $element.popover('show');
 
-					$('#modal-image').modal('show');
-				}
-			});
+    $('#button-image').on('click', function () {
+      var $button = $(this);
+      var $icon = $button.find('> i');
 
-			$element.popover('destroy');
-		});
+      $('#modal-image').remove();
 
-		$('#button-clear').on('click', function() {
-			$element.find('img').attr('src', $element.find('img').attr('data-placeholder'));
+      $.ajax({
+        url: 'index.php?route=common/filemanager&user_token=' + getURLVar('user_token') + '&target=' + $element.parent().find('input').attr('id') + '&thumb=' + $element.attr('id'),
+        dataType: 'html',
+        beforeSend: function () {
+          $button.prop('disabled', true);
+          if ($icon.length) {
+            $icon.attr('class', 'fa fa-circle-o-notch fa-spin');
+          }
+        },
+        complete: function () {
+          $button.prop('disabled', false);
 
-			$element.parent().find('input').val('');
+          if ($icon.length) {
+            $icon.attr('class', 'fa fa-pencil');
+          }
+        },
+        success: function (html) {
+          $('body').append('<div id="modal-image" class="modal">' + html + '</div>');
 
-			$element.popover('destroy');
-		});
-	});
+          $('#modal-image').modal('show');
+        }
+      });
+
+      $element.popover('destroy');
+    });
+
+    $('#button-clear').on('click', function () {
+      $element.find('img').attr('src', $element.find('img').attr('data-placeholder'));
+
+      $element.parent().find('input').val('');
+
+      $element.popover('destroy');
+    });
+  });
 });
 
 // Autocomplete */
-(function($) {
-	$.fn.autocomplete = function(option) {
-		return this.each(function() {
-			var $this = $(this);
-			var $dropdown = $('<ul class="dropdown-menu" />');
+(function ($) {
+  $.fn.autocomplete = function (option) {
+    return this.each(function () {
+      var $this = $(this);
+      var $dropdown = $('<ul class="dropdown-menu" />');
 
-			this.timer = null;
-			this.items = [];
+      this.timer = null;
+      this.items = [];
 
-			$.extend(this, option);
+      $.extend(this, option);
 
-			$this.attr('autocomplete', 'off');
+      $this.attr('autocomplete', 'off');
 
-			// Focus
-			$this.on('focus', function() {
-				this.request();
-			});
+      // Focus
+      $this.on('focus', function () {
+        this.request();
+      });
 
-			// Blur
-			$this.on('blur', function() {
-				setTimeout(function(object) {
-					object.hide();
-				}, 200, this);
-			});
+      // Blur
+      $this.on('blur', function () {
+        setTimeout(function (object) {
+          object.hide();
+        }, 200, this);
+      });
 
-			// Keydown
-			$this.on('keydown', function(event) {
-				switch(event.keyCode) {
-					case 27: // escape
-					this.hide();
-					break;
-					default:
-					this.request();
-					break;
-				}
-			});
+      // Keydown
+      $this.on('keydown', function (event) {
+        switch (event.keyCode) {
+          case 27: // escape
+            this.hide();
+            break;
+          default:
+            this.request();
+            break;
+        }
+      });
 
-			// Click
-			this.click = function(event) {
-				event.preventDefault();
+      // Click
+      this.click = function (event) {
+        event.preventDefault();
 
-				var value = $(event.target).parent().attr('data-value');
+        var value = $(event.target).parent().attr('data-value');
 
-				if (value && this.items[value]) {
-					this.select(this.items[value]);
-				}
-			}
+        if (value && this.items[value]) {
+          this.select(this.items[value]);
+        }
+      }
 
-			// Show
-			this.show = function() {
-				var pos = $this.position();
+      // Show
+      this.show = function () {
+        var pos = $this.position();
 
-				$dropdown.css({
-					top: pos.top + $this.outerHeight(),
-					left: pos.left
-				});
+        $dropdown.css({
+          top: pos.top + $this.outerHeight(),
+          left: pos.left
+        });
 
-				$dropdown.show();
-			}
+        $dropdown.show();
+      }
 
-			// Hide
-			this.hide = function() {
-				$dropdown.hide();
-			}
+      // Hide
+      this.hide = function () {
+        $dropdown.hide();
+      }
 
-			// Request
-			this.request = function() {
-				clearTimeout(this.timer);
+      // Request
+      this.request = function () {
+        clearTimeout(this.timer);
 
-				this.timer = setTimeout(function(object) {
-					object.source($(object).val(), $.proxy(object.response, object));
-				}, 200, this);
-			}
+        this.timer = setTimeout(function (object) {
+          object.source($(object).val(), $.proxy(object.response, object));
+        }, 200, this);
+      }
 
-			// Response
-			this.response = function(json) {
-				var html = '';
-				var category = {};
-				var name;
-				var i = 0, j = 0;
+      // Response
+      this.response = function (json) {
+        var html = '';
+        var category = {};
+        var name;
+        var i = 0, j = 0;
 
-				if (json.length) {
-					for (i = 0; i < json.length; i++) {
-						// update element items
-						this.items[json[i]['value']] = json[i];
+        if (json.length) {
+          for (i = 0; i < json.length; i++) {
+            // update element items
+            this.items[json[i]['value']] = json[i];
 
-						if (!json[i]['category']) {
-							// ungrouped items
-							html += '<li data-value="' + json[i]['value'] + '"><a href="#">' + json[i]['label'] + '</a></li>';
-						} else {
-							// grouped items
-							name = json[i]['category'];
-							if (!category[name]) {
-								category[name] = [];
-							}
+            if (!json[i]['category']) {
+              // ungrouped items
+              html += '<li data-value="' + json[i]['value'] + '"><a href="#">' + json[i]['label'] + '</a></li>';
+            } else {
+              // grouped items
+              name = json[i]['category'];
+              if (!category[name]) {
+                category[name] = [];
+              }
 
-							category[name].push(json[i]);
-						}
-					}
+              category[name].push(json[i]);
+            }
+          }
 
-					for (name in category) {
-						html += '<li class="dropdown-header">' + name + '</li>';
+          for (name in category) {
+            html += '<li class="dropdown-header">' + name + '</li>';
 
-						for (j = 0; j < category[name].length; j++) {
-							html += '<li data-value="' + category[name][j]['value'] + '"><a href="#">&nbsp;&nbsp;&nbsp;' + category[name][j]['label'] + '</a></li>';
-						}
-					}
-				}
+            for (j = 0; j < category[name].length; j++) {
+              html += '<li data-value="' + category[name][j]['value'] + '"><a href="#">&nbsp;&nbsp;&nbsp;' + category[name][j]['label'] + '</a></li>';
+            }
+          }
+        }
 
-				if (html) {
-					this.show();
-				} else {
-					this.hide();
-				}
+        if (html) {
+          this.show();
+        } else {
+          this.hide();
+        }
 
-				$dropdown.html(html);
-			}
+        $dropdown.html(html);
+      }
 
-			$dropdown.on('click', '> li > a', $.proxy(this.click, this));
-			$this.after($dropdown);
-		});
-	}
+      $dropdown.on('click', '> li > a', $.proxy(this.click, this));
+      $this.after($dropdown);
+    });
+  }
 })(window.jQuery);
